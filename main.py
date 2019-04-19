6bd7a4c74448ed7eaad9c9260a83f88853a24796
mrs-lizard <elizavetatk@yandex.ru>
2019-04-19 19:04:45 +0700

    Добавлены MOCK-данные

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/mock")
+def mocks():
+    """
+    Метод для MOCK-данных
+    :return: MOCK
+    """
+    news_list = [
+        {
+            'id': 1,
+            'title': "UNIX родился!",
+            'text': 'Наша жизнь кардинально изменится',
+            'date': '01.01.1980',
+            'link': ''
+        },
+        {
+            'id': 2,
+            'title': "Офигенная новость",
+            'text': 'Ура! У меня получилось сделать моки',
+            'date': '19.04.2019',
+            'link': ''
+        },
+    ]
+
+    return jsonify(items=news_list)
 
 
 app.run()
